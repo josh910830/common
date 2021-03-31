39b086376179ed767e4554f46e6755b5110621ce
suloginscene <suloginscene@gmail.com>
2021-03-31 16:20:32 +0900

    feat: Simplify ExceptionAdvice

--- src/main/java/com/github/suloginscene/exception/ExceptionAdvice.java
+++ src/main/java/com/github/suloginscene/exception/ExceptionAdvice.java
@@ -2,12 +2,9 @@ package com.github.suloginscene.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindException;
-import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import static java.util.stream.Collectors.joining;
 import static org.springframework.http.HttpStatus.FAILED_DEPENDENCY;
@@ -55,13 +52,6 @@ public class ExceptionAdvice {
                 .status(INTERNAL_SERVER_ERROR).build();
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<Void> on(Exception e) {
-        log.error(toLogString(e));
-        return ResponseEntity
-                .status(INTERNAL_SERVER_ERROR).build();
-    }
-
     private String toLogString(Exception e) {
         String className = e.getClass().getSimpleName();
         String message = e.getMessage();
@@ -79,51 +69,6 @@ public class ExceptionAdvice {
         return e.getBindingResult().getFieldErrors().stream()
                 .map(f -> f.getField() + "=" + f.getRejectedValue())
                 .collect(joining(","));
-    }
-
-
-    @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<?> on(HttpMessageNotReadableException e) {
-        if (isRequiredRequestBodyMissing(e)) {
-            return on(new RequestException("required request body is missing"));
-        }
-
-        return on(new InternalException("http message not readable while required request body is present"));
-    }
-
-    private boolean isRequiredRequestBodyMissing(HttpMessageNotReadableException e) {
-        String message = e.getMessage();
-        return message != null && message.startsWith("Required request body is missing");
-    }
-
-
-    @ExceptionHandler(MissingServletRequestParameterException.class)
-    public ResponseEntity<?> on(MissingServletRequestParameterException e) {
-        if (isRequiredMissing(e)) {
-            return on(new RequestException("required parameter is missing"));
-        }
-
-        return on(new InternalException("missing servlet request param while required parameter is present"));
-    }
-
-    private boolean isRequiredMissing(MissingServletRequestParameterException e) {
-        String message = e.getMessage();
-        return message != null && message.startsWith("Required");
-    }
-
-
-    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    public ResponseEntity<?> on(MethodArgumentTypeMismatchException e) {
-        if (isNullArgument(e)) {
-            return on(new RequestException(e.getName() + " is null"));
-        }
-
-        return on(new InternalException("method argument type mismatch while non null"));
-    }
-
-    private boolean isNullArgument(MethodArgumentTypeMismatchException e) {
-        Object value = e.getValue();
-        return value == null || value.equals("null");
     }
 
 }
