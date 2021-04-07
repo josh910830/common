c085fced6df192af4a09fbd0b220f591fe651c05
suloginscene <suloginscene@gmail.com>
2021-04-07 23:32:16 +0900

    feat: Add CamelEnum to map name CamelCase

---
+++ src/main/java/com/github/suloginscene/enumeration/CamelEnum.java
@@ -0,0 +1,31 @@
+package com.github.suloginscene.enumeration;
+
+
+public interface CamelEnum {
+
+
+    String name();
+
+
+    default String toCamel() {
+        String[] lowerTokens = name().toLowerCase().split("_");
+
+        String firstWord = lowerTokens[0];
+        String otherWords = combinedOtherWords(lowerTokens);
+
+        return firstWord + otherWords;
+    }
+
+    private String combinedOtherWords(String[] lowerTokens) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < lowerTokens.length; i++) {
+            String targetWord = lowerTokens[i];
+            String firstLetter = targetWord.substring(0, 1).toUpperCase();
+            String otherLetters = targetWord.substring(1);
+            String resultWord = firstLetter + otherLetters;
+            sb.append(resultWord);
+        }
+        return sb.toString();
+    }
+
+}
